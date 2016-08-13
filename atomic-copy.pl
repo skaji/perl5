c1f8954414d76776bc74b815453dbdb4f6f86c39
Shoichi Kaji <skaji@cpan.org>
2016-08-13 11:03:48 +0900

    atomic-copy.pl

--- atomic-copy.pl
+++ atomic-copy.pl
@@ -3,6 +3,16 @@ use 5.24.0;
 use File::Temp ();
 use File::Copy ();
 use File::Basename ();
+use Fcntl ();
+
+sub x_tempfile {
+    my $file = shift;
+    # from HTTP::Tiny::mirror()
+    my $tempfile = "$file.$$." . int(rand(2**31));
+    my $flag = Fcntl::O_EXCL() | Fcntl::O_CREAT() | Fcntl::O_WRONLY();
+    sysopen my $fh, $tempfile, $flag or return;
+    ($fh, $tempfile);
+}
 
 sub atomic_copy_p {
     my ($from, $target) = @_;
@@ -22,4 +32,13 @@ sub atomic_copy_p {
     rename $temp_name, $target;
 }
 
-atomic_copy_p $0, "file.log";
+sub atomic_copy_2 {
+    my ($from, $target) = @_;
+    my ($tempfh, $tempfile) = x_tempfile $target;
+    $tempfh or return;
+    File::Copy::copy($from, $tempfh) or return;
+    close $tempfh;
+    rename $tempfile, $target;
+}
+
+atomic_copy_2 $0, "file.log";
