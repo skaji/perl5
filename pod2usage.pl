825ced47c9db1d6ba567fd1fe0398a10b3c45cee
Shoichi Kaji <skaji@cpan.org>
2016-11-26 13:17:13 +0900

    pod2usage.pl

---
+++ pod2usage.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Pod::Usage 1.33 (); # shipt with 5.8.8
+
+Pod::Usage::pod2usage(verbose => 99, section => 'SYNOPSIS|OPTIONS|EXAMPLES');
+
+=head1 SYNOPSIS
+
+this is synopsis
+
+=head1 OPTIONS
+
+this is options
+
+=head1 EXAMPLES
+
+this is examples
+
+=cut
