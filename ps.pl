a639b5d94b54b6aa37f053033ab409c21ee73b78
Shoichi Kaji <skaji@cpan.org>
2018-01-21 15:05:21 +0900

    ps

--- ps.pl
+++ ps.pl
@@ -4,7 +4,7 @@ use warnings;
 
 sub ps {
     my %ps;
-    for my $line (`ps axo pid=,cmd=`) {
+    for my $line (`ps axo pid=,command=`) { # XXX "cmd" does not work on macOS
         chomp $line;
         $line =~ s/^\s+//;
         my ($pid, $cmd) = split /\s+/, $line, 2;
