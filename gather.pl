604e5e9a4f9ebb416a796223365e8914baa4098a
Shoichi Kaji <skaji@cpan.org>
2018-02-04 12:48:54 +0900

    gather

---
+++ gather.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+our @TAKEN;
+
+sub gather (&) {
+    my $sub = shift;
+    local @TAKEN = (@TAKEN, []);
+    $sub->();
+    @{$TAKEN[-1]};
+}
+
+sub take {
+    die "Cannot call take() outside gather block" unless @TAKEN;
+    push @{$TAKEN[-1]}, @_;
+}
+
+sub gathered {
+    die "Cannot call gathered() outside gather block" unless @TAKEN;
+    scalar @{$TAKEN[-1]};
+}
+
+my @all = gather {
+    for (1..10) {
+        take $_ if $_ % 2 == 1;
+    }
+    take "oops" unless gathered;
+};
+
+print "@all\n";
