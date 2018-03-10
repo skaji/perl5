82e7ec1895b084751af4d3b17409ada329303f24
Shoichi Kaji <skaji@cpan.org>
2018-03-10 20:33:13 +0900

    data-dumper

---
+++ data-dumper.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dumper ();
+
+sub dump_as {
+    my ($data, $name) = @_;
+    my $dumper = Data::Dumper->new([$data], [$name]);
+    $dumper->Sortkeys(1);
+    $dumper->Indent(1);
+    $dumper->{xpad} = " " x 4; # hack
+    $dumper->Useqq(1);
+    return $dumper->Dump;
+}
+
+my $data = { foo => "1\n", bar => [1..5] };
+my $str = dump_as $data, "*Foo";
+
+print "my $str";
+
+__END__
+
+my %Foo = (
+    "bar" => [
+        1,
+        2,
+        3,
+        4,
+        5
+    ],
+    "foo" => "1\n"
+);
