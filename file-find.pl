161b9b07acd90982854e44bcc80955a5ba6221d1
Shoichi Kaji <skaji@cpan.org>
2021-04-02 00:24:19 +0900

    update

--- file-find.pl
+++ file-find.pl
@@ -6,27 +6,24 @@ use File::Spec;
 sub find {
     my ($topdir, $sub) = @_;
     my @dir = ($topdir);
-    my $state = {};
-    DIRLOOP: while (defined(my $dir = shift @dir)) {
+    while (defined(my $dir = shift @dir)) {
         opendir my $dh, $dir or die "$!: $dir";
         while (defined(my $path = readdir $dh)) {
             next if $path eq "." || $path eq "..";
             $path = File::Spec->catfile($dir, $path);
             local $_ = $path;
-            my $ret = $sub->($path, $state);
-            last DIRLOOP if ref($ret) eq 'SCALAR' && !$$ret;
+            my $ret = $sub->($path);
+            return if ref($ret) eq 'SCALAR' && !$$ret;
             push @dir, $path if -d $path && -r _ && !-l $path;
         }
     }
-    return $state;
 }
 
-my $size = find ".", sub {
-    my ($path, $size) = @_;
+my $size = {};
+find ".", sub {
+    my $path = shift;
     $size->{$path} = -s $path;
-    if ($path eq ".git/refs") {
-        return \0;
-    }
+    return \0 if $path eq ".git/refs";
 };
 
 use D;
